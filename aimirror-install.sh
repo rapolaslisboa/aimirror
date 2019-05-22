cb1b1edcedbeb09e77330fbc94a1343811c18c7f
Rápolas Lisboa <36749168+rapolaslisboa@users.noreply.github.com>
2019-05-22 16:51:39 +0200

    Changes

--- aimirror-install.sh
+++ aimirror-install.sh
@@ -12,20 +12,14 @@ npm install -y
 cd
 sudo mv /home/pi/setupAIMirror/config.js /home/pi/AI-Mirror/config
 sudo mv /home/pi/setupAIMirror/asoundrc /home/pi
-sudo apt-get install python3-dev python3-venv -y
-echo "Inicio installation do Google Samples mais o ambiente virtual do Python" 
+sudo apt-get install python3-dev python3-venv -y 
 python3 -m venv env
-echo "Passou do python3 -m venv env" 
 env/bin/python -m pip install --upgrade pip setuptools wheel
 source env/bin/activate
 sudo apt-get install portaudio19-dev libffi-dev libssl-dev libmpg123-dev
-echo "Passo 1"
 python -m pip install --upgrade google-assistant-library==1.0.1
-echo "Passo 2"
 python -m pip install --upgrade google-assistant-sdk[samples]==0.5.1
-echo "Passo 3"
 sudo apt-get install python-pip
-echo "Passo do pip"
 pip freeze | grep RPi
 sudo apt-get install python-dev python-rpi.gpio
 pip install RPi.GPIO
