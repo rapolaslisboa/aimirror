8639842d406665160f54a3740cf17b92d023167d
Rápolas Lisboa <36749168+rapolaslisboa@users.noreply.github.com>
2019-05-22 16:03:42 +0200

    Adicionado código para mover asoundrc de diretório

--- aimirror-install.sh
+++ aimirror-install.sh
@@ -10,6 +10,7 @@ cd AI-Mirror
 npm install -y
 cd
 sudo mv /setupAIMirror/config.js /pi/AI-Mirror/config
+sudo mv /setupAIMirror/asoundrc /pi
 sudo apt-get install python3-dev python3-venv -y
 python3 -m venv env
 env/bin/python -m pip install --upgrade pip setuptools wheel
