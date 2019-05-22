6d55ee1e30165d6dfd85904a863202475b453c7e
Rápolas Lisboa <36749168+rapolaslisboa@users.noreply.github.com>
2019-05-22 16:26:04 +0200

    Arrumado os caminhos para mover os arquivos

--- aimirror-install.sh
+++ aimirror-install.sh
@@ -10,8 +10,8 @@ git clone https://github.com/MichMich/MagicMirror.git AI-Mirror
 cd AI-Mirror
 npm install -y
 cd
-sudo mv /setupAIMirror/config.js /pi/AI-Mirror/config
-sudo mv /setupAIMirror/asoundrc /pi
+sudo mv /home/pi/setupAIMirror/config.js /home/pi/AI-Mirror/config
+sudo mv /home/pi/setupAIMirror/asoundrc /home/pi
 sudo apt-get install python3-dev python3-venv -y
 python3 -m venv env
 env/bin/python -m pip install --upgrade pip setuptools wheel
@@ -25,8 +25,8 @@ sudo apt-get install python-dev python-rpi.gpio
 pip install RPi.GPIO
 sudo apt-get install picap -y
 deactivate
-sudo mv /setupAIMirror/autostart /etc/xdg/lxsession/LXDE-pi
-sudo mv /setupAIMirror/hotword.py /pi/env/lib/python3.5/site-packages/googlesamples/assistant/library
+sudo mv /home/pi/setupAIMirror/autostart /etc/xdg/lxsession/LXDE-pi
+sudo mv /home/pi/setupAIMirror/hotword.py /home/pi/env/lib/python3.5/site-packages/googlesamples/assistant/library
 
 
 
