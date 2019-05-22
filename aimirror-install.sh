d93fe0f090d8172cd95d099f83cdfc9d9b5e8fa3
Rápolas Lisboa <36749168+rapolaslisboa@users.noreply.github.com>
2019-05-22 15:57:07 +0200

    Atualizando script para instalação

--- aimirror-install.sh
+++ aimirror-install.sh
@@ -9,7 +9,7 @@ git clone https://github.com/MichMich/MagicMirror.git AI-Mirror
 cd AI-Mirror
 npm install -y
 cd
-sudo mv config.js  /home/pi/AI-Mirror/
+sudo mv /setupAIMirror/config.js /pi/AI-Mirror/config
 sudo apt-get install python3-dev python3-venv -y
 python3 -m venv env
 env/bin/python -m pip install --upgrade pip setuptools wheel
@@ -23,9 +23,8 @@ sudo apt-get install python-dev python-rpi.gpio
 pip install RPi.GPIO
 sudo apt-get install picap -y
 deactivate
-sudo mv config.js /pi/AI-Mirror/config
-sudo mv autostart /etc/xdg/lxsession/LXDE-pi
-sudo mv hotword.py /pi/env/lib/python3.5/site-packages/googlesamples/assistant/library
+sudo mv /setupAIMirror/autostart /etc/xdg/lxsession/LXDE-pi
+sudo mv /setupAIMirror/hotword.py /pi/env/lib/python3.5/site-packages/googlesamples/assistant/library
 
 
 
