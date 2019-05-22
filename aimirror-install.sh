988003a2506429e43e8eb8ed4414862fcf9df21d
Rápolas Lisboa <36749168+rapolaslisboa@users.noreply.github.com>
2019-05-22 16:40:32 +0200

    Remover diretório setupAIMirror e prints com echo

--- aimirror-install.sh
+++ aimirror-install.sh
@@ -13,13 +13,19 @@ cd
 sudo mv /home/pi/setupAIMirror/config.js /home/pi/AI-Mirror/config
 sudo mv /home/pi/setupAIMirror/asoundrc /home/pi
 sudo apt-get install python3-dev python3-venv -y
+echo "Inicio installation do Google Samples mais o ambiente virtual do Python" 
 python3 -m venv env
+echo "Passou do python3 -m venv env" 
 env/bin/python -m pip install --upgrade pip setuptools wheel
 source env/bin/activate
 sudo apt-get install portaudio19-dev libffi-dev libssl-dev libmpg123-dev -y
+echo "Passo 1"
 python -m pip install --upgrade google-assistant-library==1.0.1 -y
+echo "Passo 2"
 python -m pip install --upgrade google-assistant-sdk[samples]==0.5.1 -y
+echo "Passo 3"
 sudo apt-get install python-pip
+echo "Passo do pip"
 pip freeze | grep RPi
 sudo apt-get install python-dev python-rpi.gpio
 pip install RPi.GPIO
@@ -27,6 +33,7 @@ sudo apt-get install picap -y
 deactivate
 sudo mv /home/pi/setupAIMirror/autostart /etc/xdg/lxsession/LXDE-pi
 sudo mv /home/pi/setupAIMirror/hotword.py /home/pi/env/lib/python3.5/site-packages/googlesamples/assistant/library
+sudo rm -r setupAIMirror
 
 
 
