41bfb4c9c21867de6492a275e24eb8780ea03126
Rápolas Lisboa <36749168+rapolaslisboa@users.noreply.github.com>
2019-05-23 16:18:26 +0200

    Código atualizado

--- aimirror-install.sh
+++ aimirror-install.sh
@@ -21,7 +21,7 @@ sudo apt-get install python3-dev python3-venv -y
 python3 -m venv env
 env/bin/python -m pip install --upgrade pip setuptools wheel
 source env/bin/activate
-sudo apt-get install portaudio19-dev libffi-dev libssl-dev libmpg123-dev
+sudo apt-get install portaudio19-dev libffi-dev libssl-dev libmpg123-dev -y
 python -m pip install --upgrade google-assistant-library==1.0.1
 python -m pip install --upgrade google-assistant-sdk[samples]==0.5.1
 sudo apt-get install python-pip
@@ -34,7 +34,8 @@ sudo mv /home/pi/setupAIMirror/autostart /etc/xdg/lxsession/LXDE-pi
 sudo mv /home/pi/setupAIMirror/hotword.py /home/pi/env/lib/python3.5/site-packages/googlesamples/assistant/library
 sudo rm -r setupAIMirror
 sudo chmod +x /etc/xdg/lxsession/LXDE-pi/autostart
-
+sudo chmod +x /etc/xdg/lxseddion/LXDE-pi/autostart
+sudo rm -r setupAIMirror
 
 
 
