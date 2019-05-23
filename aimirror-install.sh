ee306a060ef6d8866560b3d69bf1951d7b1a3329
Rápolas Lisboa <36749168+rapolaslisboa@users.noreply.github.com>
2019-05-23 12:18:54 +0200

    23/05

--- aimirror-install.sh
+++ aimirror-install.sh
@@ -12,6 +12,10 @@ npm install -y
 cd
 sudo mv /home/pi/setupAIMirror/config.js /home/pi/AI-Mirror/config
 sudo mv /home/pi/setupAIMirror/asoundrc /home/pi
+sudo mv /home/pi/setupAIMirror/autostartsassistant.sh /home/pi
+sudo mv /home/pi/setupAIMirror/autostartmirror.sh /home/pi
+sudo chmod +x /home/pi/autostartsassistant.sh
+sudo chmod +x /home/pi/autostartmirror.sh
 sudo apt-get install python3-dev python3-venv -y 
 python3 -m venv env
 env/bin/python -m pip install --upgrade pip setuptools wheel
@@ -28,7 +32,8 @@ deactivate
 sudo mv /home/pi/setupAIMirror/autostart /etc/xdg/lxsession/LXDE-pi
 sudo mv /home/pi/setupAIMirror/hotword.py /home/pi/env/lib/python3.5/site-packages/googlesamples/assistant/library
 sudo rm -r setupAIMirror
-sudo chdmod +x /etc/xdg/lxsession/LXDE-pi/autostart
+sudo chmod +x /etc/xdg/lxsession/LXDE-pi/autostart
+
 
 
 
